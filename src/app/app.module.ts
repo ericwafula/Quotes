a40a9937c9e9a946111b126ce712b0e7f274ee95
Eric Wafula <eric.wafula@student.moringaschool.com>
2021-07-16 21:06:59 +0300

    added left section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CardComponent } from './card/card.component';
+import { LeftSectionComponent } from './left-section/left-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    CardComponent
+    CardComponent,
+    LeftSectionComponent
   ],
   imports: [
     BrowserModule,
