4f6893d16fc782e1dba53c3e2b97a52db8ff1ca7
Eric Wafula <eric.wafula@student.moringaschool.com>
2021-07-16 21:45:43 +0300

    added Quotes constructor

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,13 @@
 export class Quotes {
+    quote: string;
+    author: string;
+    contributor: string;
+    date: string;
+
+    constructor(quote: string, author: string, contributor: string){
+        this.quote = quote;
+        this.author = author;
+        this.contributor = contributor;
+        this.date = "";
+    }
 }
