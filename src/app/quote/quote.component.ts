a231254f615516560c1c02f7654069c0610b4df4
Eric Wafula <eric.wafula@student.moringaschool.com>
2021-07-18 20:08:03 +0300

    removed loop

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -12,8 +12,7 @@ export class QuoteComponent implements OnInit {
   constructor() { }
 
   quotes:Quotes[] = [
-    new Quotes("When something is important enough, you do it even if the odds are not in your favor.", "Elon Musk", "Eric Wathome", new Date(2020, 11, 14)),
-    new Quotes("If you get up in the morning and think the future is going to be better, it is a bright day. Otherwise, it's not.", "Elon Musk", "Eric Wathome", new Date(2020, 11, 14)),
+    new Quotes("When something is important enough, you do it even if the odds are not in your favor.", "Elon Musk", "Eric Wathome", new Date(2020, 11, 14))
   ]
 
   addNewQuote(quote:Quotes){
@@ -32,18 +31,6 @@ export class QuoteComponent implements OnInit {
   increaseLikes(toIncreaseLikes: boolean, index: number){
     if (toIncreaseLikes){
       this.quotes[index].upvote++;
-      let getLikes = <any>document.getElementsByClassName("like");
-  
-      // console.log(this.quotes[index].upvote)
-      console.log(typeof getLikes)
-      for (let i = 0; i < getLikes.length; i++){
-        let elementOne = getLikes[i].children[1].innerText[i];
-        
-        console.log(elementOne);
-        // console.log(elementTwo);
-        // console.log(`element 1 = ${elementOne + 1}`);
-        // console.log(`element 2 = ${elementTwo + 1}`);
-      }
     }
   }
 
